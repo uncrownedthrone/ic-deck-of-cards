c7a29be3a8e21cdd4137af5b2711c6ad5e55a4ae
Justin Swiger <justin.swiger@me.com>
2019-11-07 11:50:43 -0500

    TODO add js from class

--- public/main.js
+++ public/main.js
@@ -1,7 +1,43 @@
+const qs = (e) => document.querySelector(e)
+
+const ranks = [
+  { name: 'A' },
+  { name: '2' },
+  { name: '3' },
+  { name: '4' },
+  { name: '5' },
+  { name: '6' },
+  { name: '7' },
+  { name: '8' },
+  { name: '9' },
+  { name: '10' },
+  { name: 'J' },
+  { name: 'Q' },
+  { name: 'K' }
+]
+const suits = ['Hearts ♥️', 'Clubs ♣️', 'Spades ♠️', 'Diamonds ♦️']
+
+const deck = []
+
 const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+  for (let i = 0; i < suits.length; i++) {
+    for (let j = 0; j < ranks.length; j++) {
+      const card = {
+        rank: ranks[j],
+        suits: suits[i],
+        value: values[j]
+      }
+      deck.push(card)
+    }
   }
+  for (let i = 0; i < deck.length; i++) {
+    const j = Math.floor(Math.random() * 52)
+    const temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
+  }
+  console.log(deck)
 }
 
 document.addEventListener('DOMContentLoaded', main)
+qs('button').addEventListener('click', dealACard)
